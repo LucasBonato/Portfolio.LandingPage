77515601cd689a42ab5527e803a28aaec7b6bda9
LucasBonato <lucas.perez.bonato@gmail.com>
2025-11-07 03:33:47 -0300

    [FIX] Remove use query

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwind from "@tailwindcss/vite";
 import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
+import { dirname } from "path";
+import path from "path";
 
 // Recreate __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
@@ -10,12 +11,9 @@ const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwind()],
-  optimizeDeps: {
-    include: ["@tanstack/react-query"],
-  },
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
