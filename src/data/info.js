b357de1acf38b0896e2899e41198e14264462c22
LucasBonato <lucas.perez.bonato@gmail.com>
2025-11-07 03:40:02 -0300

    [FEAT] Information about me

---
+++ src/data/info.js
@@ -0,0 +1,21 @@
+import { Github, Linkedin, Mail } from "lucide-react";
+
+const info = [
+  {
+    icon: Github,
+    href: "https://github.com/LucasBonato",
+    label: "GitHub",
+  },
+  {
+    icon: Linkedin,
+    href: "https://linkedin.com/in/lucas-bonato-78a22b233/",
+    label: "LinkedIn",
+  },
+  {
+    icon: Mail,
+    href: "mailto:lucas.perez.bonato@gmail.com",
+    label: "Email",
+  },
+];
+
+export default info;
